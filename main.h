6129b034b113b8468119bf363c1d2ed508d18a68
Roceso1337 <Roceso1337@gmail.com>
2016-10-19 23:02:38 -0400

    Changed vector to queue

--- main.h
+++ main.h
@@ -1,21 +1,19 @@
 #ifndef _MAIN
 #define _MAIN
 
-#include <queue>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string>
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include "process.h"
 
 struct process;
 
-void FCFS(const std::vector<process>&);
-void SJF(const std::vector<process>&);
-void roundRobin(const std::vector<process>&);
+void FCFS(const std::queue<process>&);
+void SJF(const std::queue<process>&);
+void roundRobin(const std::queue<process>&);
 void err(const char*);
 
 #endif
