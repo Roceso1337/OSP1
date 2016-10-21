3fc9b2a4510bcc33ff49401621051c7b0fd284cd
Daniel Sze <danssze@gmail.com>
2016-10-20 23:09:00 -0400

    finished SJF

--- process.h
+++ process.h
@@ -33,6 +33,7 @@ public:
 
 	//<proc-id>|<initial-arrival-time>|<cpu-burst-time>|<num-bursts>|<io-time>
 	process();
+    process(const process&);
 	process(std::string, int, int, int, int);
 	static void parse(std::vector<std::string>&, std::deque<process>&);
     std::string getID();
@@ -66,7 +67,7 @@ public:
 
 
 	static bool IOComp(const process &p1, const process &p2){
-        return p1.ioEndTime > p2.ioEndTime;
+        return p1.ioEndTime < p2.ioEndTime;
 	}
 
 	static std::string printTime(int i){
